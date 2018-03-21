d6d22639a23b17f07991aebfe9406a1a24ed34f2
Yak Jun Xiang <snowypowers@gmail.com>
2018-03-20 23:36:40 -0700

    chore(all): scaffold using vue-cli

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,86 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div>
+    <input type="text" v-model="input" placeholder="Input"/>
+    <button v-on:click="clear">X</button>
+    </div>
+    <div>
+      <button v-on:click="reverseHex">Reverse Hex</button>
+      <button v-on:click="account">Account</button>
+      <button v-on:click="int2fixed8">Number -> Fixed8</button>
+      <button v-on:click="fixed82int">Fixed8 -> Number</button>
+    </div>
+    <div>
+      <p id="output"> {{ output }} </p>
+    </div>
   </div>
 </template>
 
 <script>
+import { u, wallet } from '@cityofzion/neon-js'
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      input: "",
+      output: "Output"
+    };
+  },
+  methods: {
+    clear: function() {
+      this.input = "";
+    },
+    account: function() {
+      this.output = "Invalid Input!";
+      try {
+        const acct = new wallet.Account(this.input);
+        this.output = "";
+        if (this.input === "")
+          this.output += `This is randomly generated using a secure random generator.\n`;
+        this.output += `Address: ${acct.address}\n`;
+        this.output += `ScriptHash: ${acct.scriptHash}\n`;
+        this.output += `Public Key: ${acct.publicKey}\n`;
+        this.output += `Private Key: ${acct.privateKey}\n`;
+        this.output += `WIF: ${acct.WIF}\n`;
+      } catch (err) {
+        return
+      }
+    },
+    reverseHex: function() {
+      if (!u.isHex(this.input)) {
+        this.output = "Invalid Input! Expected a hexstring!";
+        return;
+      }
+      this.output = u.reverseHex(this.input);
+    },
+    int2fixed8: function() {
+      try {
+        this.output = new u.Fixed8(this.input).toHex();
+      } catch (err) {
+        this.output = "Invalid Input! Expected a number!";
+      }
+    },
+    fixed82int: function() {
+      if (!u.isHex(this.input)) {
+        this.output = "Invalid Input! Expected a hexstring!";
+        return;
+      }
+      const f = u.Fixed8.fromHex(this.input);
+      this.output = f.toNumber();
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#output {
+  white-space: pre-line
+}
+
 h3 {
   margin: 40px 0 0;
 }
