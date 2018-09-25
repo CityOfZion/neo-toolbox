3ab2bc8e508e3739dae38c598218e5bf68be5821
Jeroen Peeters <jeroen.peeters@evance.be>
2018-09-25 08:38:34 +0200

    feat(conversions): convert scriptHash to Python/C#

--- src/lib/conversions.js
+++ src/lib/conversions.js
@@ -1,14 +1,22 @@
 import { u, wallet } from "@cityofzion/neon-js";
+import bsqsq from "bsqsq";
+
+import convertToCSharp from "./../lib/converToCSharp";
 
 export const account = input => {
   let output = "";
   try {
     const acct = new wallet.Account(input);
+    const python = bsqsq(acct.scriptHash, true);
+    const cSharp = convertToCSharp(acct.scriptHash);
+
     output += `<p class="tag">Address</p><p>${acct.address}</p><br />`;
     output += `<p class="tag">Scripthash</p><p>${acct.scriptHash}</p><br />`;
+    output += `<p class="tag">Python Account</p><p>${python}</p><br />`;
+    output += `<p class="tag">C# Account</p><p>${cSharp}</p><br />`;
     output += `<p class="tag">Public Key</p><p>${acct.publicKey}</p><br />`;
-    output += `<p class="tag">Private Key</p><p>${acct.privateKey}</p><br />`;
     output += `<p class="tag">WIF</p><p>${acct.WIF}</p><br />`;
+    output += `<p class="tag">Private Key</p><p>${acct.privateKey}</p><br />`;
     return output;
   } catch (err) {
     return output !== "" ? output : "Invalid Input!";
