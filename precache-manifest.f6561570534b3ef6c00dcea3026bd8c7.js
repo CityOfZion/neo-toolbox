b1e023738d2739d4e6ef1968bfd138f1f1e3257e
Jeroen Peeters <jeroen.peeters@evance.be>
2018-09-25 08:46:42 +0200

    Updates

---
+++ precache-manifest.f6561570534b3ef6c00dcea3026bd8c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04274f31d7507bf95d0b",
+    "url": "/neo-toolbox/js/vendor.04274f31.js"
+  },
+  {
+    "revision": "88bc385880927113dda6",
+    "url": "/neo-toolbox/js/manifest.88bc3858.js"
+  },
+  {
+    "revision": "c2257bee0edd46d6f0bf",
+    "url": "/neo-toolbox/js/app.c2257bee.js"
+  },
+  {
+    "revision": "b7d8154902ad50d92fd8ac07b1c633c6",
+    "url": "/neo-toolbox/index.html"
+  },
+  {
+    "revision": "c2257bee0edd46d6f0bf",
+    "url": "/neo-toolbox/css/app.a0262ae4.css"
+  }
+];
